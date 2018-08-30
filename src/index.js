5e06dbc261aa4f8ad573063165a8b1fef5859b31
BrunoMCerqueira <brunocerqueiram@gmail.com>
2018-08-30 16:46:17 -0300

    react-redux installed, provider wrapping, connect with dispatch and state created in component App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer)
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>    
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
